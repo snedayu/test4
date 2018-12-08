95e0d4167a175bb1c8ff122952f5c6da59506c6c
snedayu <43269123+snedayu@users.noreply.github.com>
2018-12-09 00:36:04 +0800

    Add files via upload

---
+++ serverTest4/src/server/server.java
@@ -0,0 +1,50 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.net.ssl.SSLServerSocketFactory;
+
+public class server {
+     
+    static final int port = 8088;
+ 
+    public static void main(String[] args) {
+         
+         
+        SSLServerSocketFactory sslServerSocketFactory = 
+                (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+         
+        try {
+            ServerSocket sslServerSocket = 
+                    sslServerSocketFactory.createServerSocket(port);
+            System.out.println("SSL ServerSocket started");
+            System.out.println(sslServerSocket.toString());
+             
+            Socket socket = sslServerSocket.accept();
+            System.out.println("ServerSocket accepted");
+             
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            try (BufferedReader bufferedReader = 
+                    new BufferedReader(
+                            new InputStreamReader(socket.getInputStream()))) {
+                String line;
+                while((line = bufferedReader.readLine()) != null){
+                    System.out.println(line);
+                    out.println(line);
+                }
+            }
+            System.out.println("Closed");
+             
+        } catch (IOException ex) {
+            Logger.getLogger(server.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+    }
+     
+}
